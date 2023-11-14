842199f3f52928fafef5f47490f7d86bae12db03
Mateusz Tywczyński <mateusz.tywczynski@gmail.com>
2023-11-14 20:32:37 +0100

    RTLTesting basics

---
+++ src/App/Exercises/ReactExercises/RTLTesting/router-data.jsx
@@ -0,0 +1,10 @@
+import { RTLTesting } from '.';
+
+export const RTLTestingMetaData = {
+  path: 'rtl-testing',
+  date: null,
+  linkLabel: 'Testowanie przy użyciu RTL',
+  blockNo: 45,
+  element: <RTLTesting />,
+  tags: ['components', 'rtl', 'react-testing-library'],
+};
